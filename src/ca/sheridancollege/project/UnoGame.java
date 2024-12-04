c302c2a4977b621c75ac2527d6a53c6df31306d0
JordanSarmiento <115197188+JordanSarmiento@users.noreply.github.com>
2024-12-04 16:33:38 -0500

    UnoGame class asks how many players want to play from 2-4 players, takes the names of each player to notify whose turn it is and then starts Uno Card game from Game class logic

--- src/ca/sheridancollege/project/UnoGame.java
+++ src/ca/sheridancollege/project/UnoGame.java
@@ -4,10 +4,26 @@
  */
 package ca.sheridancollege.project;
 
+import java.util.Scanner;
+
 /**
  *
  * @author jordansarmiento
  */
 public class UnoGame {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number of players (2-4): ");
+        int numOfPlayers = input.nextInt();
+        input.nextLine(); 
+        
+        Game unoGame = new Game("UNO Game");
+        for (int i = 1; i <= numOfPlayers; i++) {
+            System.out.println("Enter name for Player " + i + ": ");
+            String playerName = input.nextLine();
+            unoGame.addPlayer(new UnoPlayer(playerName)); 
+        } 
+        unoGame.startGame();
+    }
 }
